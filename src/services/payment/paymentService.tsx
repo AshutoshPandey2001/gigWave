ef4383d4585e02fdc59592e0f9668e83f9de6ea5
Aniruddh <ani.thakor@gmail.com>
2023-10-30 20:09:04 +0530

    implementation of webview in ios and add payment onboard APIs

---
+++ src/services/payment/paymentService.tsx
@@ -0,0 +1,37 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import { API_BASE_URL } from "../apiConstant";
+
+export const onboardUser = async (userData: any, token: any): Promise<any> => {
+    try {
+        const response = await axios({
+            method: 'post',
+            url: `${API_BASE_URL}/onboard_user?user_id=${userData.user_id}`,
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        });
+        return response.data; // Return the response data
+    } catch (error) {
+        console.error('error', error);
+
+        throw error; // Rethrow the error to be caught by the caller
+    }
+}
+export const checkAccountStatus = async (userData: any, token: any): Promise<any> => {
+    try {
+        const response = await axios({
+            method: 'get',
+            url: `${API_BASE_URL}/check_account_status/${userData.user_id}`,
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        });
+        return response.data; // Return the response data
+    } catch (error) {
+        console.error('error', error);
+
+        throw error; // Rethrow the error to be caught by the caller
+    }
+}
